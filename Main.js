cc2fdf8be2412eb3b45a4db5284a8aa6d90bcf00
Imran232 <pataanimran1947@gmail.com>
2023-06-16 21:03:05 +0530

    Add These

--- Main.js
+++ Main.js
@@ -1,6 +1,6 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
-
+var filter = document.getElementById('filter');
 
 // Form submit event
 form.addEventListener('submit', addItem);
@@ -8,6 +8,9 @@ form.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 // Edit event
 itemList.addEventListener('click',editItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
+
 // Add item
 function addItem(e){
   e.preventDefault();
@@ -52,4 +55,21 @@ function removeItem(e){
     }
   }
 }
+// Filter Items
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      var itemName1=item.childNodes[1].textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1||itemName1.toLowerCase().indexOf(text)!=-1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
 
