e53a08e85b34089f04fa54ae43206db67997c1f4
Imran232 <pataanimran1947@gmail.com>
2023-06-17 13:21:34 +0530

    Add Final Work

--- main2.js
+++ main2.js
@@ -3,6 +3,19 @@ function myfunc(event){
     var Name=document.getElementById("name").value;
     var Email=document.getElementById("email").value;
 
-    localStorage.setItem('Name',Name);
-    localStorage.setItem('Email',Email);
+    //localStorage.setItem('Name',Name);
+    //localStorage.setItem('Email',Email);
+    var obj={
+        Name,
+        Email
+
+    }
+    localStorage.setItem(obj.Email,JSON.stringify(obj))
+    showUserOnScreen(obj)
+}
+function showUserOnScreen(obj){
+    var parentElem=document.getElementById('my-form');
+    var ChildElem=document.createElement('li');
+    ChildElem.textContent=obj.Name+'-'+obj.Email;
+    parentElem.appendChild(ChildElem)
 }
